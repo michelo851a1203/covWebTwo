2f1f816daa6a5711b991a4b48c8c8c8d57651b15
MichaelHo <michelo851a1203@gmail.com>
2020-09-15 17:42:30 +0800

    fix template and add append

---
+++ src/components/MainForm.vue
@@ -0,0 +1,168 @@
+<template>
+  <div>
+    <div class="self-start ml-10 mb-4">
+      <label>{{ title }}</label>
+    </div>
+    <div
+      v-for="item in iData"
+      :key="item.id"
+      class="w-full ml-10"
+      :class="{ 
+        'mb-4':item.margin === 'small',
+        'mb-10':item.amrgin === 'medium',
+        'mb-12':item.amrgin === 'large',
+        'self-start' : item.type === 'ddl' || item.type === 'checkbox' || item.type === 'radio' 
+      }"
+    >
+      <!-- textbox -->
+      <input
+        v-model.trim="formDataRef[outputFortitle ? item.title : item.id]"
+        v-if="item.type === 'text'"
+        class="w-4/5 border-b-2 border-gray-600 placeholder-gray-600 bg-transparent sm:border-gray-400 px-4 pt-1 focus:outline-none"
+        :placeholder="item.title"
+        type="text"
+      />
+      <!-- password -->
+      <input
+        v-model.trim="formDataRef[outputFortitle ? item.title : item.id]"
+        v-if="item.type === 'password'"
+        class="w-4/5 border-b-2 border-gray-600 placeholder-gray-600 bg-transparent sm:border-gray-400 px-4 pt-1 focus:outline-none"
+        :placeholder="item.title"
+        type="password"
+      />
+      <!-- dropdownlist -->
+      <div v-if="item.type === 'ddl'">
+        <label for>{{ item.title }}</label>
+        <select
+          v-model="formDataRef[outputFortitle ? item.title : item.id]"
+          class="ml-4 border rounded border-gray-500 px-2 py-1"
+        >
+          <option class="bg-white" value>please Select</option>
+          <option
+            v-for="ddlItem in item.ddl"
+            :key="ddlItem.value"
+            class="bg-white"
+            :value="ddlItem.value"
+          >{{ ddlItem.title }}</option>
+        </select>
+      </div>
+      <!-- checkbox -->
+      <div v-if="item.type === 'checkbox'" class="flex items-center">
+        <input
+          v-model.trim="formDataRef[outputFortitle ? item.title : item.id]"
+          :true-value="(item.cb && item.cb.trueValue) ? item.cb.trueValue : true"
+          :false-value="(item.cb && item.cb.falseValue) ? item.cb.falseValue : false"
+          :id="item.id + item.title"
+          type="checkbox"
+        />
+        <label class="cursor-pointer text-xs ml-2" :for="item.id + item.title">{{ item.title }}</label>
+      </div>
+      <!-- radiobox (when spare time) -->
+    </div>
+
+    <div class="w-4/5 mb-2 flex justify-around">
+      <button
+        @click="btnClick(item.emitname,item.needSendData)"
+        v-for="item in funcbtn"
+        :key="item.id"
+        :class="{ 
+          'bg-gray-700 hover:bg-gray-900': item.style === 'normal',
+          'bg-green-700 hover:bg-green-900': item.style === 'info',
+          'bg-blue-700 hover:bg-blue-900': item.style === 'primary',
+          'bg-red-700 hover:bg-red-900': item.style === 'danger',
+          'bg-orange-700 hover:bg-orange-900': item.style === 'warn',
+        }"
+        class="focus:outline-none text-white font-medium py-2 px-4 rounded"
+      >{{ item.title }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+// ============================================================================
+// ** element **
+// <mainform
+//       v-if="boardTitle !== '' && boardiData.length > 0"
+//       :outputFortitle="true"
+//       :iData="boardiData"
+//       :funcbtn="boardFunc"
+//       @clear="clearFrom"
+//       @AddUser="adminAddUser"
+//     ></mainform>
+
+// ** props **
+
+// title:"maintitle"
+// outputFortitle: true
+// iData: [
+//   {
+//     id:"id",
+//     title: "placeHolder",
+//     type: "label,text,password,ddl,datetimepicker,checkbox,radio,",
+//     margin: "small,medium,large",
+//     ep: "bold,none",
+//     ddl: [{ value, title }], // if type is ddl need this one
+//     cb: { trueValue: "", falseValue: "" }, // if is checkbox apply this
+//   },
+// ];
+// funcbtn: [
+//   {
+//     id: "id",
+//     title: "btn title",
+//     style: "normal,info,primary,danger,warn",
+//     // normal:gray,info:green,primary:blue,danger:red,warn:yellow
+//     emitname: "do some action outside",
+//     needSendData:false, // if yes will emit all form
+//   },
+// ];
+
+// ============================================================================
+import { ref } from "vue";
+export default {
+  name: "mainform",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    outputFortitle: {
+      type: Boolean,
+      default: false,
+    },
+    iData: {
+      default: () => [],
+      type: Array,
+    },
+    funcbtn: {
+      default: () => [],
+      type: Array,
+    },
+  },
+  setup(props, { emit }) {
+    const formDataRef = ref({});
+
+    props.iData.forEach((item) => {
+      if (item.type === "ddl") {
+        formDataRef.value[props.outputFortitle ? item.title : item.id] = "";
+      }
+    });
+
+    const btnClick = (emitName, needSendData = true) => {
+      if (needSendData) {
+        props.iData.forEach((item) => {
+          const key = props.outputFortitle ? item.title : item.id;
+          if (Object.keys(formDataRef.value).includes(key)) {
+            return;
+          }
+          formDataRef.value[key] = "";
+        });
+
+        emit(emitName, formDataRef.value);
+        return;
+      }
+      emit(emitName);
+    };
+    return { formDataRef, btnClick };
+  },
+};
+</script>
--- src/components/UserTable.vue
+++ src/components/UserTable.vue
@@ -1,21 +1,34 @@
 <template>
   <div>
     <dashboardtable
+      v-if="boardTitle === '' && boardiData.length === 0"
       addBtnName="add user"
+      @create="addUserfunc"
       :func="['update','delete','create']"
       :iData="userMainList"
     ></dashboardtable>
+    <mainform
+      v-if="boardTitle !== '' && boardiData.length > 0"
+      :outputFortitle="true"
+      :iData="boardiData"
+      :funcbtn="boardFunc"
+      @clear="clearFrom"
+      @AddUser="adminAddUser"
+    ></mainform>
   </div>
 </template>
 
 <script>
 import dashboardtable from "@/components/DashboardTable.vue";
+import mainform from "@/components/MainForm.vue";
 import Admin from "@/api/Admin.js";
+import { reactive, toRefs } from "vue";
 
 export default {
   name: "usertable",
   components: {
     dashboardtable,
+    mainform,
   },
   async setup() {
     const adminModules = Admin();
@@ -24,7 +37,80 @@ export default {
       console.error("adminModules.getMainList : get data");
       return;
     }
-    return { ...adminModules };
+
+    const funcBoardRef = reactive({
+      boardTitle: "",
+      boardiData: [],
+      boardFunc: [],
+    });
+    const clearFrom = () => {
+      funcBoardRef.boardTitle = "";
+      funcBoardRef.boardiData = [];
+      funcBoardRef.boardFunc = [];
+    };
+
+    const addUserfunc = () => {
+      funcBoardRef.boardTitle = "add user";
+      funcBoardRef.boardiData = [
+        {
+          id: 1,
+          title: "username",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 2,
+          title: "password",
+          type: "password",
+          margin: "small",
+        },
+        {
+          id: 3,
+          title: "email",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 4,
+          title: "displayName",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 5,
+          title: "role",
+          type: "ddl",
+          margin: "small",
+          ddl: [
+            {
+              value: 2,
+              title: "validator",
+            },
+            {
+              value: 3,
+              title: "Test Center",
+            },
+          ],
+        },
+      ];
+      funcBoardRef.boardFunc = [
+        {
+          id: 1,
+          title: "cancel",
+          style: "normal",
+          emitname: "clear",
+          needSendData: false,
+        },
+        {
+          id: 2,
+          title: "add user",
+          style: "info",
+          emitname: "AddUser",
+        },
+      ];
+    };
+
+    return { ...adminModules, ...toRefs(funcBoardRef), clearFrom, addUserfunc };
   },
 };
 </script>
