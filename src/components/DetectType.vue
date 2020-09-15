931bacc1b2492a81cff2cfb3f0e995122f10b059
MichaelHo <michelo851a1203@gmail.com>
2020-09-15 18:48:32 +0800

    20200915 add all and wait for test

--- src/components/DetectType.vue
+++ src/components/DetectType.vue
@@ -2,20 +2,35 @@
   <div>
     <dashboardtable
       addBtnName="add detect type"
+      @create="addDetectfunc"
+      @update="updateDetectFunc"
+      @delete="DeleteDetectType"
       :func="['update','delete','create']"
       :iData="detectTypeMainList"
     ></dashboardtable>
+    <mainform
+      v-if="boardTitle !== '' && boardiData.length > 0"
+      :outputFortitle="true"
+      :iData="boardiData"
+      :funcbtn="boardFunc"
+      @clear="clearFrom"
+      @AddUser="addDataAction"
+      @UpdateUser="updateDataAction"
+    ></mainform>
   </div>
 </template>
 
 <script>
 import dashboardtable from "@/components/DashboardTable.vue";
+import mainform from "@/components/MainForm.vue";
 import DoctorType from "@/api/DoctorType.js";
+import { reactive, toRefs } from "vue";
 
 export default {
   name: "usertable",
   components: {
     dashboardtable,
+    mainform,
   },
   async setup() {
     const doctorTypeModules = DoctorType();
@@ -24,7 +39,124 @@ export default {
       console.error("getMainList : get data");
       return;
     }
-    return { ...doctorTypeModules };
+
+    const funcBoardRef = reactive({
+      boardTitle: "",
+      boardiData: [],
+      boardFunc: [],
+    });
+
+    const clearFrom = () => {
+      funcBoardRef.boardTitle = "";
+      funcBoardRef.boardiData = [];
+      funcBoardRef.boardFunc = [];
+    };
+
+    const addDetectfunc = () => {
+      funcBoardRef.boardTitle = "add detecttype";
+      funcBoardRef.boardiData = [
+        {
+          id: 1,
+          title: "name",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 2,
+          title: "normalRange",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 3,
+          title: "cctNo",
+          type: "text",
+          margin: "small",
+        },
+      ];
+      funcBoardRef.boardFunc = [
+        {
+          id: 1,
+          title: "cancel",
+          style: "normal",
+          emitname: "clear",
+          needSendData: false,
+        },
+        {
+          id: 2,
+          title: "add detect type",
+          style: "info",
+          emitname: "AddUser",
+        },
+      ];
+    };
+
+    const updateDetectFunc = (detectTypeId) => {
+      funcBoardRef.boardTitle = "update detect type";
+      funcBoardRef.boardiData = [
+        {
+          id: 1,
+          title: "detectTypeId",
+          type: "label",
+          label: detectTypeId,
+          margin: "small",
+        },
+        {
+          id: 2,
+          title: "normalRange",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 3,
+          title: "cctNo",
+          type: "text",
+          margin: "small",
+        },
+      ];
+      funcBoardRef.boardFunc = [
+        {
+          id: 1,
+          title: "cancel",
+          style: "normal",
+          emitname: "clear",
+          needSendData: false,
+        },
+        {
+          id: 2,
+          title: "update detect type",
+          style: "info",
+          emitname: "UpdateUser",
+        },
+      ];
+    };
+
+    const addDataAction = async (iData) => {
+      const { success } = await doctorTypeModules.AddDetectType(iData);
+      if (!success) {
+        console.log("add user fail");
+        return;
+      }
+      clearFrom();
+    };
+
+    const updateDataAction = async (iData) => {
+      const { success } = await doctorTypeModules.UpdateDetectType(iData);
+      if (!success) {
+        console.log("update user fail");
+        return;
+      }
+      clearFrom();
+    };
+    return {
+      ...doctorTypeModules,
+      ...toRefs(funcBoardRef),
+      clearFrom,
+      addDetectfunc,
+      updateDetectFunc,
+      addDataAction,
+      updateDataAction,
+    };
   },
 };
 </script>
--- src/components/DoctorTable.vue
+++ src/components/DoctorTable.vue
@@ -3,19 +3,34 @@
     <dashboardtable
       addBtnName="add doctor"
       :func="['update','delete','create']"
+      @create="addDoctorfunc"
+      @update="updateDoctorFunc"
+      @delete="doctorDeleteUser"
       :iData="doctorMainList"
     ></dashboardtable>
+    <mainform
+      v-if="boardTitle !== '' && boardiData.length > 0"
+      :outputFortitle="true"
+      :iData="boardiData"
+      :funcbtn="boardFunc"
+      @clear="clearFrom"
+      @AddUser="addDataAction"
+      @UpdateUser="updateDataAction"
+    ></mainform>
   </div>
 </template>
 
 <script>
 import dashboardtable from "@/components/DashboardTable.vue";
+import mainform from "@/components/MainForm.vue";
 import Doctor from "@/api/Doctor.js";
+import { reactive, toRefs } from "vue";
 
 export default {
   name: "usertable",
   components: {
     dashboardtable,
+    mainform,
   },
   async setup() {
     const doctorModules = Doctor();
@@ -24,7 +39,161 @@ export default {
       console.error("doctorModules.getMainList : get data");
       return;
     }
-    return { ...doctorModules };
+
+    const funcBoardRef = reactive({
+      boardTitle: "",
+      boardiData: [],
+      boardFunc: [],
+    });
+
+    const clearFrom = () => {
+      funcBoardRef.boardTitle = "";
+      funcBoardRef.boardiData = [];
+      funcBoardRef.boardFunc = [];
+    };
+
+    const addDoctorfunc = () => {
+      funcBoardRef.boardTitle = "add doctor";
+      funcBoardRef.boardiData = [
+        {
+          id: 1,
+          title: "clinic",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 2,
+          title: "name",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 3,
+          title: "email",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 4,
+          title: "address",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 5,
+          title: "city",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 6,
+          title: "zip",
+          type: "text",
+          margin: "small",
+        },
+      ];
+      funcBoardRef.boardFunc = [
+        {
+          id: 1,
+          title: "cancel",
+          style: "normal",
+          emitname: "clear",
+          needSendData: false,
+        },
+        {
+          id: 2,
+          title: "add user",
+          style: "info",
+          emitname: "AddDoctor",
+        },
+      ];
+    };
+
+    const updateDoctorFunc = (doctorId) => {
+      funcBoardRef.boardTitle = "update Doctor Info";
+      funcBoardRef.boardiData = [
+        {
+          id: 1,
+          title: "doctorId",
+          type: "label",
+          label: doctorId,
+          margin: "small",
+        },
+        {
+          id: 2,
+          title: "clinic",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 3,
+          title: "email",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 4,
+          title: "address",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 5,
+          title: "city",
+          type: "text",
+          margin: "small",
+        },
+        {
+          id: 6,
+          title: "zip",
+          type: "text",
+          margin: "small",
+        },
+      ];
+      funcBoardRef.boardFunc = [
+        {
+          id: 1,
+          title: "cancel",
+          style: "normal",
+          emitname: "clear",
+          needSendData: false,
+        },
+        {
+          id: 2,
+          title: "update Info",
+          style: "info",
+          emitname: "UpdateUser",
+        },
+      ];
+    };
+
+    const addDataAction = async (iData) => {
+      const { success } = await doctorModules.doctorAddUser(iData);
+      if (!success) {
+        console.log("add user fail");
+        return;
+      }
+      clearFrom();
+    };
+
+    const updateDataAction = async (iData) => {
+      const { success } = await doctorModules.doctorUpdateUser(iData);
+      if (!success) {
+        console.log("update user fail");
+        return;
+      }
+      clearFrom();
+    };
+
+    return {
+      ...doctorModules,
+      ...toRefs(funcBoardRef),
+      clearFrom,
+      addDoctorfunc,
+      updateDoctorFunc,
+      addDataAction,
+      updateDataAction,
+    };
   },
 };
 </script>
