6d200c7e3d3029a27a626e29db3028c1f08592e0
MichaelHo <michelo851a1203@gmail.com>
2020-12-08 21:09:09 +0800

    deploy

---
+++ precache-manifest.02fb5332b993af3227e9203567b34980.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d09acb05c65088083543b3a8ae70b88",
+    "url": "/covWebTwo/index.html"
+  },
+  {
+    "revision": "c6cf5162479292b55b8e",
+    "url": "/covWebTwo/js/AdminDashboard.23ff55a9.js"
+  },
+  {
+    "revision": "e1d4e9e64d486046be42",
+    "url": "/covWebTwo/js/AdminDashboard~DirectorDashBoard~DoctorDashboard~Index~TestTypeDashBoard.489d8cd6.js"
+  },
+  {
+    "revision": "8290ef1a29df47623dc9",
+    "url": "/covWebTwo/js/AdminDashboard~DirectorDashBoard~DoctorDashboard~TestTypeDashBoard.066eef17.js"
+  },
+  {
+    "revision": "9fe1c298859838cdacc9",
+    "url": "/covWebTwo/js/DirectorDashBoard.6eae335d.js"
+  },
+  {
+    "revision": "4b03cfb71bc312a24937",
+    "url": "/covWebTwo/js/DoctorDashboard.c52e207a.js"
+  },
+  {
+    "revision": "2c235e06c41a0dc95582",
+    "url": "/covWebTwo/js/Index.a3c70e8d.js"
+  },
+  {
+    "revision": "c3091cf523b3369159f8",
+    "url": "/covWebTwo/js/Report.434165f3.js"
+  },
+  {
+    "revision": "db8f523ea94c417a7233",
+    "url": "/covWebTwo/js/Report~Scan.3a63e387.js"
+  },
+  {
+    "revision": "ae465b1317fe40f171e7",
+    "url": "/covWebTwo/js/Scan.fb69d051.js"
+  },
+  {
+    "revision": "1648683c998cbdbff869",
+    "url": "/covWebTwo/js/TestTypeDashBoard.4f1f3806.js"
+  },
+  {
+    "revision": "4c39b57ad4978f498bc4",
+    "url": "/covWebTwo/js/VerifyReport.2b35b774.js"
+  },
+  {
+    "revision": "0f1ba5442b7bc3d23959",
+    "url": "/covWebTwo/js/about.917a4cbf.js"
+  },
+  {
+    "revision": "9f9ad726631b4e1e7525",
+    "url": "/covWebTwo/js/app.0eb11248.js"
+  },
+  {
+    "revision": "71d345aaa8a06662657e",
+    "url": "/covWebTwo/js/chunk-vendors.4286b01c.js"
+  },
+  {
+    "revision": "628468cd2e4984d20666b1a5c69ca4d4",
+    "url": "/covWebTwo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/covWebTwo/robots.txt"
+  }
+]);
